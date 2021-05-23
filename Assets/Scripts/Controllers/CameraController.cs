9a7922c4b0a6cfdb4e87d8e5586333352e6e220b
Nastya <isoant2s@gmail.com>
2021-05-23 15:48:15 +0300

    Created UI for pause

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -9,11 +9,11 @@ public class CameraController : MonoBehaviour
 
     void Start()
     {
-        offset = transform.position - player.position; // calculate the offset
+        offset = transform.position - player.position; // calculate the offset, plus new vector to make the camera a bit closer
     }
 
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, new Vector3(player.position.x, 0, 0) + offset, Time.deltaTime); // using Lerp to make movement smooth
+        transform.position = Vector3.Lerp(transform.position, new Vector3(player.position.x + 2, 0, 0) + offset, Time.deltaTime); // using Lerp to make movement smooth
     }
 }
---
+++ Assets/Scripts/Controllers/GameUIController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUIController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pausePanel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        pausePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
